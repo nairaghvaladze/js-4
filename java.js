2176638c6f2ac1898f88e815bf75eebbed4bf520
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-04-25 21:46:23 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+var c=document.getElementsByTagName ('BODY')[0];
+c.style.backgroundColor="pink";
+var a = document.getElementById('btn1');
+a.style.color="green";
+a.addEventListener('click', function onClick( ) {
+ 
+  document.body.style.backgroundColor = 'green';
+
+});
+
+var b = document.getElementById('btn2');
+b.style.color="blue";
+b.addEventListener('click', function onClick() {
+ 
+  document.body.style.backgroundColor = 'blue';
+
+});
+
